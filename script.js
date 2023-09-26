e2883e8ef1372f30f12a5c43e67868449fea15e1
me <me@gmail.com>
2023-09-26 09:46:50 +0200

    sixth commit: JS Rebuild Arrays

--- script.js
+++ script.js
@@ -102,8 +102,31 @@ function updateDOM() {
   });
 
   // Run getSavedColumns only once, Update Local Storage
+  updatedOnLoad = true;
+  updateSavedColumns();
+}
 
-
+// Allow Arrays to Reflect Drag and Drop Items
+function rebuildArrays() {
+  console.log(backlogList.children);
+  console.log(progressList.children);
+  backlogListArray = [];
+  for (let i=0; i<backlogList.children.length; i++) {
+    backlogListArray.push(backlogList.children[i].textContent);
+  }
+  progressListArray = [];
+  for (let i=0; i<progressList.children.length; i++) {
+    progressListArray.push(progressList.children[i].textContent);
+  }
+  completeListArray = [];
+  for (let i=0; i<completeList.children.length; i++) {
+    completeListArray.push(completeList.children[i].textContent);
+  }
+  onHoldListArray = [];
+  for (let i=0; i<onHoldList.children.length; i++) {
+    onHoldListArray.push(onHoldList.children[i].textContent);
+  }
+  updateDOM();
 }
 
 // When Item Starts Dragging
@@ -134,6 +157,7 @@ function drop(e) {
   // Add ITem to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  rebuildArrays();
 }
 
 // On Load
